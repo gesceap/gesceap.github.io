0e6379c438cee7e37a5a5ada15a7d43d21ffef59
Sam Wray <sam@wray.pro>
2020-07-20 20:19:09 +0100

    feat(layout): makes social media icons global

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,8 +1,56 @@
 <template>
-  <div v-if="!webaudioShieldAccepted" class="web-audio-sheild">
-    <button @click="clickBegin">Begin</button>
+  <div id="app">
+    <RouterView v-if="webaudioShieldAccepted" />
+
+    <div id="sm">
+      <a
+        href="https://www.youtube.com/channel/UChvIb5lU1CswBTno6BCGG4Q"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img alt="YouTube" src="/img/yt.png" />
+      </a>
+      <a
+        href="https://www.facebook.com/gesceap/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img alt="Facebook" src="/img/fb.png" />
+      </a>
+      <a
+        href="https://twitter.com/gesceap_"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img alt="Twitter" src="/img/tw.png" />
+      </a>
+      <a
+        href="https://gesceap.bandcamp.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img alt="Bandcamp" src="/img/bc.png" />
+      </a>
+      <a
+        href="https://github.com/gesceap"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img alt="GitHub" src="/img/gh.png" />
+      </a>
+      <a
+        href="https://soundcloud.com/gesceap"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img alt="SoundCloud" src="/img/sc.png" />
+      </a>
+    </div>
+
+    <div v-if="!webaudioShieldAccepted" class="web-audio-sheild">
+      <button @click="clickBegin">Begin</button>
+    </div>
   </div>
-  <RouterView v-else />
 </template>
 
 <script>
@@ -26,15 +74,43 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+html,
+body {
+  background-color: #000;
+  height: 100%;
+  margin: 0;
+}
+
+#app {
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#sm {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+
+#sm a img {
+  width: 50px;
+  height: 50px;
+  margin: 5px;
+}
+
 .web-audio-sheild {
+  background-color: #000;
   position: fixed;
   top: 0;
   left: 0;
   bottom: 0;
   right: 0;
   width: 100%;
-  right: 100%;
+  height: 100%;
 
   display: flex;
   justify-content: center;
--- project/src/components/routes/SonicDesert.vue
+++ project/src/components/routes/SonicDesert.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <audio autoplay loop ref="wind">
       <source src="~../../../assets/sonic-desert/wind.mp3" type="audio/mpeg" />
       <source src="~../../../assets/sonic-desert/wind.ogg" type="audio/ogg" />
@@ -25,51 +25,6 @@
           alt="Mystic Rose"
         />
       </button>
-    </div>
-
-    <div id="sm">
-      <a
-        href="https://www.youtube.com/channel/UChvIb5lU1CswBTno6BCGG4Q"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img alt="YouTube" src="/img/yt.png" />
-      </a>
-      <a
-        href="https://www.facebook.com/gesceap/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img alt="Facebook" src="/img/fb.png" />
-      </a>
-      <a
-        href="https://twitter.com/gesceap_"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img alt="Twitter" src="/img/tw.png" />
-      </a>
-      <a
-        href="https://gesceap.bandcamp.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img alt="Bandcamp" src="/img/bc.png" />
-      </a>
-      <a
-        href="https://github.com/gesceap"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img alt="GitHub" src="/img/gh.png" />
-      </a>
-      <a
-        href="https://soundcloud.com/gesceap"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img alt="SoundCloud" src="/img/sc.png" />
-      </a>
     </div>
   </div>
 </template>
@@ -99,33 +54,6 @@ export default {
 </script>
 
 <style>
-html,
-body {
-  background-color: #000;
-  height: 100%;
-  margin: 0;
-}
-
-#app {
-  height: 100%;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-#sm {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-}
-
-#sm a img {
-  width: 50px;
-  height: 50px;
-  margin: 5px;
-}
-
 .background-container,
 .foreground-container {
   position: absolute;
